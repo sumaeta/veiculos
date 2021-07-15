405c1f8a568b97155cd47fab60c8b3e6eb4fcd38
sumaeta <brenosumaeta33@gmail.com>
2021-07-15 11:48:46 -0300

    Entidade usuario

--- src/main/java/com/api/veiculos/model/Usuario.java
+++ src/main/java/com/api/veiculos/model/Usuario.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Usuario {
@@ -13,15 +14,51 @@ public class Usuario {
     private Long id;
 
     private String nome;
+    private String email;
+    private String cpf;
+    private String dataNascimento;
 
     public Usuario(){
     }
 
-    public Usuario(String nome) {
+    public Usuario(Long id, String nome, String email, String cpf, String dataNascimento) {
+        this.id = id;
         this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id.equals(usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
